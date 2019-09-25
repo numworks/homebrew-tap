f134c26f70a3fee8237d6de787126657e92a290a
Romain Goyet <romain.goyet@numworks.com>
2019-09-25 17:21:53 +0200

    Create an arm-none-eabi-gcc formula

---
+++ arm-none-eabi-gcc.rb
@@ -0,0 +1,15 @@
+class ArmNoneEabiGcc < Formula
+  desc "GNU Arm Embedded Toolchain"
+  homepage "https://developer.arm.com/tools-and-software/open-source-software/developer-tools/gnu-toolchain/gnu-rm"
+  url "https://armkeil.blob.core.windows.net/developer/Files/downloads/gnu-rm/8-2019q3/RC1.1/gcc-arm-none-eabi-8-2019-q3-update-mac.tar.bz2"
+  version "8-2019q3-update"
+  sha256 "fc235ce853bf3bceba46eff4b95764c5935ca07fc4998762ef5e5b7d05f37085"
+
+  def install
+    prefix.install Dir.glob("*")
+  end
+
+  test do
+    system "#{bin}/arm-none-eabi-gcc --version"
+  end
+end
