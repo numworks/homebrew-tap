0df93a740540dbf39cf0818a6562c6ddd855f548
Romain Goyet <romain.goyet@numworks.com>
2019-09-26 13:31:16 +0200

    Add a dependency on imagemagick
    It's used in the macOS and iOS builds

--- numworks-sdk.rb
+++ numworks-sdk.rb
@@ -5,10 +5,11 @@ class NumworksSdk < Formula
   sha256 "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855"
   version "1.0"
 
-  depends_on "pkg-config"
   depends_on "arm-none-eabi-gcc"
   depends_on "freetype"
+  depends_on "imagemagick"
   depends_on "libpng"
+  depends_on "pkg-config"
   depends_on "python"
 
   def install
