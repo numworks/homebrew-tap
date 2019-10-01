ef0bc2bec357fc63195646759daa98899d9de793
Romain Goyet <romain.goyet@numworks.com>
2019-10-01 17:02:56 +0200

    Rename the package to epsilon-sdk

--- epsilon-sdk.rb
+++ epsilon-sdk.rb
@@ -1,5 +1,5 @@
-class NumworksSdk < Formula
-  desc "NumWorks SDK"
+class EpsilonSdk < Formula
+  desc "Epsilon SDK"
   homepage "https://www.numworks.com/resources/engineering/software/"
   url "file:///dev/null"
   sha256 "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855"
