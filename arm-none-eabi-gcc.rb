913ec4fbb96f7f66f81804f9442b49034bd63c63
Romain Goyet <romain.goyet@numworks.com>
2019-11-05 11:59:57 +0100

    Use a locally-hosted gcc toolchain

--- arm-none-eabi-gcc.rb
+++ arm-none-eabi-gcc.rb
@@ -1,7 +1,7 @@
 class ArmNoneEabiGcc < Formula
   desc "GNU Arm Embedded Toolchain"
   homepage "https://developer.arm.com/tools-and-software/open-source-software/developer-tools/gnu-toolchain/gnu-rm"
-  url "https://armkeil.blob.core.windows.net/developer/Files/downloads/gnu-rm/8-2019q3/RC1.1/gcc-arm-none-eabi-8-2019-q3-update-mac.tar.bz2"
+  url "https://github.com/numworks/homebrew-tap/releases/download/gcc-arm-2019-q3/gcc-arm-none-eabi-8-2019-q3-update-mac.tar.bz2"
   version "8-2019q3-update"
   sha256 "fc235ce853bf3bceba46eff4b95764c5935ca07fc4998762ef5e5b7d05f37085"
 
