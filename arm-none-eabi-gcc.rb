82efdde92a4f132d969350c54ddf5bcdc822328a
Romain Goyet <romain.goyet@numworks.com>
2020-11-26 15:56:33 -0500

    Update toolchain to 2020q2

--- arm-none-eabi-gcc.rb
+++ arm-none-eabi-gcc.rb
@@ -1,9 +1,9 @@
 class ArmNoneEabiGcc < Formula
   desc "GNU Arm Embedded Toolchain"
   homepage "https://developer.arm.com/tools-and-software/open-source-software/developer-tools/gnu-toolchain/gnu-rm"
-  url "https://github.com/numworks/homebrew-tap/releases/download/gcc-arm-2019-q4/gcc-arm-none-eabi-9-2019-q4-major-mac.tar.bz2"
-  version "9-2019q4-update"
-  sha256 "1249f860d4155d9c3ba8f30c19e7a88c5047923cea17e0d08e633f12408f01f0"
+  url "https://github.com/numworks/homebrew-tap/releases/download/gcc-arm-2020-q2/gcc-arm-none-eabi-9-2020-q2-update-mac.tar.bz2"
+  version "9-2020q2"
+  sha256 "bbb9b87e442b426eca3148fa74705c66b49a63f148902a0ea46f676ec24f9ac6"
 
   def install
     prefix.install Dir.glob("*")
