84d2fead4a7be674276859d1de9d68fb20ba40b4
Émilie Feral <emilie.feral@numworks.com>
2021-08-30 11:53:14 +0200

    Add python lz4 library dependency

--- epsilon-sdk.rb
+++ epsilon-sdk.rb
@@ -10,10 +10,16 @@ class EpsilonSdk < Formula
   depends_on "imagemagick"
   depends_on "libpng"
   depends_on "pkg-config"
-  depends_on "python"
+  depends_on "python@3.8"
   depends_on "libusb"
 
+  resource "lz4" do
+    url "https://files.pythonhosted.org/packages/d9/c5/080234f5b6b698f56339edf7715d9256eca4eb3d35b36893227c399e69f9/lz4-3.1.3.tar.gz"
+    sha256 "081ef0a3b5941cb03127f314229a1c78bd70c9c220bb3f4dd80033e707feaa18"
+  end
+
   def install
+    resource("lz4").stage { system "python3", *Language::Python.setup_install_args(libexec/"vendor") }
     bin.mkpath
   end
 end
